0a36196eec436ca9648cf7b856fb98030ce1d262
frozenhelium <ankit.mehta@togglecorp.com>
2022-06-01 11:39:45 +0545

    Fix character encoding

--- webpack.config.js
+++ webpack.config.js
@@ -144,7 +144,6 @@ module.exports = () => {
                 // NOTE: we do not need to use this html on production
                 minify: false,
                 meta: {
-                    charset: 'UTF-8',
                     viewport: 'width=device-width, initial-scale=1.0',
                     description: pkg.description,
                     referrer: 'origin',
